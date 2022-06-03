9c6926b81a897d987280d4fe81b3fba6bf98bb32
Lucas Stofella <lucas.stofella2@havan.com.br>
2022-06-03 18:20:17 -0300

    Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,22 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({showProfileData = true}: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Lucas Stofella</Text>
-        <Text color="gray.300" fontSize="small" >lucas_stofella@hotmail.com</Text>
-      </Box>
+      {
+        showProfileData && (
+          <Box mr="4" textAlign="right">
+            <Text>Lucas Stofella</Text>
+            <Text color="gray.300" fontSize="small" >
+              lucas_stofella@hotmail.com
+            </Text>
+          </Box>
+        )
+      }
 
       <Avatar size="md" name="Lucas Stofella" src="https://avatars.githubusercontent.com/u/43644875?v=4"/>
     </Flex>
