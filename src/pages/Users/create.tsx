ccedec4256ed24937a2354975b24d4264b05297d
Lucas Stofella <lucas.stofella2@havan.com.br>
2022-06-10 17:05:35 -0300

    Cadastro responsivo

--- src/pages/Users/create.tsx
+++ src/pages/Users/create.tsx
@@ -11,7 +11,7 @@ export default function CreateUser() {
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6" >
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+        <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
           <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
           
           <Divider my="6" borderColor="gray.700"/>
@@ -19,7 +19,7 @@ export default function CreateUser() {
           <VStack spacing="8">
             <SimpleGrid
               minChildWidth="240px"
-              spacing="8"
+              spacing={["6", "8"]}
               w="100%"
             >
               <Input name="name" label="Nome completo" />
@@ -28,7 +28,7 @@ export default function CreateUser() {
 
             <SimpleGrid
               minChildWidth="240px"
-              spacing="8"
+              spacing={["6", "8"]}
               w="100%"
             >
               <Input name="password" type="password" label="Senha" />
@@ -36,7 +36,7 @@ export default function CreateUser() {
             </SimpleGrid>
           </VStack>
 
-          <Flex mt="8" justify="flex-end">
+          <Flex mt="8" justify={["center", "flex-end"]}>
             <HStack spacing="4">
               <Button colorScheme="whiteAlpha">Cancelar</Button>
               <Button colorScheme="pink">Salvar</Button>
