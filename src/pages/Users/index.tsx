4bc0a63b748990afa3c4f3bcea68efc612c0c0c7
Lucas Stofella <lucas.stofella2@havan.com.br>
2022-07-15 15:28:56 -0300

    Componente paginação + troca de pagina

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -6,9 +6,11 @@ import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 import { useUsers } from "../../services/hooks/useUsers";
+import { useState } from "react";
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useUsers();
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isFetching, error } = useUsers(page);
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -68,7 +70,7 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map(user => {
+                  {data.users.map(user => {
                     return (
                       <Tr key={user.id}>
                       <Td px={["4", "4", "6"]} color="gray.300" width="8">
@@ -100,7 +102,11 @@ export default function UserList() {
                 </Tbody>
               </Table>
 
-              <Pagination />
+              <Pagination
+                totalCountOfRegisters={data.totalCount}
+                currentPage= {page}
+                onPageChange={setPage}
+              />
             </>
           )}
         </Box>
