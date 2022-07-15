d88841d3702037d0b059cf802f01a159f085cd23
Lucas Stofella <lucas.stofella2@havan.com.br>
2022-07-15 18:20:38 -0300

    Utilizando mutations

--- src/pages/Users/create.tsx
+++ src/pages/Users/create.tsx
@@ -3,10 +3,14 @@ import Link from "next/link";
 import { SubmitHandler, useForm } from 'react-hook-form'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
+import { useMutation } from 'react-query'
 
 import { Input } from "../../components/Form/input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from "next/router";
 
 type CreateUserFormData = {
   name: string;
@@ -22,16 +26,32 @@ const createUserFormSchema = yup.object().shape({
 })
 
 export default function CreateUser() {
+  const router = useRouter();
+
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post('users', {
+      user: {
+        ...user,
+        created_at: new Date()
+      }
+    })
+      return response.data.user;
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('users')
+    }
+  });
+
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormSchema)
   });
 
   const { errors } = formState;
 
-  const handleCreateuser:  SubmitHandler<CreateUserFormData> = async (values) => {
-    await new Promise(resolve => setTimeout(resolve, 2000)); 
+  const handleCreateUser:  SubmitHandler<CreateUserFormData> = async (values) => {
+    await createUser.mutateAsync(values);
 
-    console.log(values)
+    router.push('/users')
   }
 
   return (
@@ -47,7 +67,7 @@ export default function CreateUser() {
           borderRadius={8} 
           bg="gray.800" 
           p={["6", "8"]} 
-          onSubmit={handleSubmit(handleCreateuser)}
+          onSubmit={handleSubmit(handleCreateUser)}
         >
           <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
           
