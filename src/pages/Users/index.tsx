56b89cec0813a1ff6c8852b879c9d15fe8169aa7
Lucas Stofella <lucas.stofella2@havan.com.br>
2022-07-15 13:49:58 -0300

    Criando hook de listagem

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,34 +1,14 @@
 import Link from "next/link";
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
-import { useQuery } from 'react-query'
 
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json();
-
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric',
-        })
-      }
-    });
-
-    return users;
-  }, {
-    staleTime: 1000 * 5, // 5 seconds
-  })
+  const { data, isLoading, isFetching, error } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
